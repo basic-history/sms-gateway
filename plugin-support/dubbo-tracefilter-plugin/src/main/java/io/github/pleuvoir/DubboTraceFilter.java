9e99d6a197c1f2bb48dbe70a42edcd9850d84a66
pleuvoir <pleuvior@foxmail.com>
2018-10-22 18:18:52 +0800

    :blush: 处理日志记录插件异常的情况

--- plugin-support/dubbo-tracefilter-plugin/src/main/java/io/github/pleuvoir/DubboTraceFilter.java
+++ plugin-support/dubbo-tracefilter-plugin/src/main/java/io/github/pleuvoir/DubboTraceFilter.java
@@ -9,6 +9,7 @@ import com.alibaba.dubbo.rpc.Invoker;
 import com.alibaba.dubbo.rpc.Result;
 import com.alibaba.dubbo.rpc.RpcContext;
 import com.alibaba.dubbo.rpc.RpcException;
+import com.alibaba.dubbo.rpc.RpcResult;
 import com.alibaba.dubbo.rpc.service.GenericService;
 import com.alibaba.fastjson.JSON;
 
@@ -17,20 +18,24 @@ public class DubboTraceFilter implements Filter {
 	private static Logger logger = LoggerFactory.getLogger(DubboTraceFilter.class);
 
 	public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+		
 		try {
 			FilterDesc filterReq = new FilterDesc();
 			filterReq.setInterfaceName(invocation.getInvoker().getInterface().getName());
 			filterReq.setMethodName(invocation.getMethodName());
 			filterReq.setArgs(invocation.getArguments());
 			logger.debug("dubbo 请求数据:" + JSON.toJSONString(filterReq));
+			// 调用
 			Result result = invoker.invoke(invocation);
 			if (result.hasException() && invoker.getInterface() != GenericService.class) {
-				logger.warn("dubbo 执行异常", result.getException());
+				Throwable exception = result.getException();
+				logger.warn("dubbo 执行异常", exception);
+				return new RpcResult(exception);
 			} else {
 				FilterDesc filterRsp = new FilterDesc();
 				filterRsp.setMethodName(invocation.getMethodName());
 				filterRsp.setInterfaceName(invocation.getInvoker().getInterface().getName());
-				filterRsp.setArgs(new Object[] { result.getValue() });
+				filterRsp.setArgs(new Object[]{result.getValue()});
 				logger.debug("dubbo 执行成功，返回数据" + JSON.toJSONString(filterRsp));
 			}
 			return result;
@@ -49,23 +54,28 @@ public class DubboTraceFilter implements Filter {
 		private String methodName;
 		// 参数
 		private Object[] args;
-		
+
 		// getter and setter
 		public String getInterfaceName() {
 			return interfaceName;
 		}
+
 		public String getMethodName() {
 			return methodName;
 		}
+
 		public Object[] getArgs() {
 			return args;
 		}
+
 		public void setInterfaceName(String interfaceName) {
 			this.interfaceName = interfaceName;
 		}
+
 		public void setMethodName(String methodName) {
 			this.methodName = methodName;
 		}
+
 		public void setArgs(Object[] args) {
 			this.args = args;
 		}
